cd2e287b975eb2202e5726e62048a5a7a3082b2e
WikiGenius <elyamani.muhammed.eng@gmail.com>
2024-03-23 23:37:36 +0200

    Add SensorInfoPublisher

--- hrwros_week1/hrwros/hrwros_week1/hrwros_week1/template_publisher_script.py
+++ hrwros_week1/hrwros/hrwros_week1/hrwros_week1/template_publisher_script.py
@@ -2,35 +2,57 @@
 
 import rclpy
 from rclpy.node import Node
-from std_msgs.msg import String
+from hrwros_msgs.msg import SensorInformation
+import random
 
-class SimplePublisher(Node):
+
+class SensorInfoPublisher(Node):
 
     def __init__(self):
-        super().__init__('node_1')
-        self.publisher = self.create_publisher(String, 'topic_1', 10)
+        super().__init__('sensor_info_publisher')
+        self.publisher = self.create_publisher(
+            SensorInformation, 'sensor_info', 10)
         timer_period = 1  # seconds
-        self.timer = self.create_timer(timer_period, self.publish_message)
-        self.topic_content = "Welcome to Hello (Real) World with ROS!!!"
+        self.timer = self.create_timer(timer_period, self.publish_sensor_info)
 
-    def publish_message(self):
-        msg = String()
-        msg.data = self.topic_content
+    def publish_sensor_info(self):
+        msg = SensorInformation()
+
+        # Assuming you want to set the frame_id to some value,
+        # and the range to a fixed or randomly generated number for demonstration.
+        msg.sensor_data.header.stamp = self.get_clock().now().to_msg()
+        msg.sensor_data.header.frame_id = "distance_sensor_frame"
+        msg.sensor_data.radiation_type = msg.sensor_data.ULTRASOUND
+        msg.sensor_data.field_of_view = 0.5  # Example value in radians
+        msg.sensor_data.min_range = 0.02  # Example min range in meters
+        msg.sensor_data.max_range = 2.0  # Example max range in meters
+
+        # Simulate sensor data with randomized range values
+        simulated_range = random.uniform(
+            msg.sensor_data.min_range,  # Minimum range in meters
+            msg.sensor_data.max_range)  # Maximum range in meters
+
+        # Example distance measurement in meters
+        msg.sensor_data.range = simulated_range
+        
+        msg.maker_name = "The Ultrasound Company"
+        msg.part_number = 123456
+
         self.publisher.publish(msg)
-        self.get_logger().info('Publishing: "%s"' % msg.data)
+        self.get_logger().info(
+            f'Publishing Sensor Information: Range = {round(msg.sensor_data.range, 3)} meters')
+
 
 def main(args=None):
     rclpy.init(args=args)
 
-    simple_publisher = SimplePublisher()
+    sensor_info_publisher = SensorInfoPublisher()
 
-    rclpy.spin(simple_publisher)
+    rclpy.spin(sensor_info_publisher)
 
-    # Destroy the node explicitly
-    # (optional - otherwise it will be done automatically
-    # when the garbage collector destroys the node object)
-    simple_publisher.destroy_node()
+    sensor_info_publisher.destroy_node()
     rclpy.shutdown()
+
 
 if __name__ == '__main__':
     main()
--- hrwros_week1/hrwros/hrwros_week1/hrwros_week1/template_subscriber_script.py
+++ hrwros_week1/hrwros/hrwros_week1/hrwros_week1/template_subscriber_script.py
@@ -2,32 +2,37 @@
 
 import rclpy
 from rclpy.node import Node
-from std_msgs.msg import String
+from hrwros_msgs.msg import SensorInformation
 
-class StringSubscriber(Node):
+
+class SensorInfoSubscriber(Node):
 
     def __init__(self):
-        super().__init__('node_2')
+        super().__init__('sensor_info_subscriber')
         self.subscription = self.create_subscription(
-            String,
-            'topic_1',
-            self.string_listener_callback,
+            SensorInformation,
+            'sensor_info',
+            self.sensor_info_callback,
             10)
         self.subscription  # prevent unused variable warning
 
-    def string_listener_callback(self, msg):
-        self.get_logger().info('I heard: "%s"' % msg.data)
+    def sensor_info_callback(self, msg):
+        sensor_range = msg.sensor_data.range
+        self.get_logger().info(
+            f'Received sensor data: Range = {round(sensor_range, 3)} meters')
+
 
 def main(args=None):
     rclpy.init(args=args)
 
-    string_subscriber = StringSubscriber()
+    sensor_info_subscriber = SensorInfoSubscriber()
 
-    rclpy.spin(string_subscriber)
+    rclpy.spin(sensor_info_subscriber)
 
-    # Cleanup and shutdown
-    string_subscriber.destroy_node()
+    # Cleanup
+    sensor_info_subscriber.destroy_node()
     rclpy.shutdown()
+
 
 if __name__ == '__main__':
     main()
