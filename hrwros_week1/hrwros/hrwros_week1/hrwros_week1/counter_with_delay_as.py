05d1d592b469ebe5fe7cd751497dfe7ba8017ccf
WikiGenius <elyamani.muhammed.eng@gmail.com>
2024-03-24 07:12:08 +0200

    Fix action

--- hrwros_week1/hrwros/hrwros_week1/hrwros_week1/counter_with_delay_as.py
+++ hrwros_week1/hrwros/hrwros_week1/hrwros_week1/counter_with_delay_as.py
@@ -3,13 +3,13 @@
 import rclpy
 from rclpy.action import ActionServer
 from rclpy.node import Node
-
+import time 
 from hrwros_msgs.action import CounterWithDelay
-from hrwros_msgs.msg import CounterWithDelayFeedback, CounterWithDelayResult
 
 class CounterWithDelayActionServer(Node):
     def __init__(self):
         super().__init__('counter_with_delay_action_server')
+        self.declare_parameter('counter_delay', 1.0)  # Default delay value
         self._action_server = ActionServer(
             self,
             CounterWithDelay,
@@ -18,26 +18,20 @@ class CounterWithDelayActionServer(Node):
 
     def execute_callback(self, goal_handle):
         self.get_logger().info("Executing goal...")
-        feedback_msg = CounterWithDelayFeedback()
-        result = CounterWithDelayResult()
+        feedback_msg = CounterWithDelay.Feedback()
+        result = CounterWithDelay.Result()
 
-        counter_delay_value = 1.0  # Default delay value
-        # Check if the parameter exists and get its value
-        if self.has_parameter('counter_delay'):
-            counter_delay_value = self.get_parameter('counter_delay').value
-
-        delay_rate = self.create_rate(1.0 / counter_delay_value)
-
+        counter_delay_value = self.get_parameter('counter_delay').value
         success = True
         for counter_idx in range(0, goal_handle.request.num_counts):
             if goal_handle.is_cancel_requested:
                 goal_handle.canceled()
                 self.get_logger().info('Goal canceled')
                 return CounterWithDelay.Result()
-
+    
             feedback_msg.counts_elapsed = counter_idx
             goal_handle.publish_feedback(feedback_msg)
-            delay_rate.sleep()
+            time.sleep(counter_delay_value)  # Directly use counter_delay_value for sleep
 
         if success:
             result.result_message = "Successfully completed counting."
