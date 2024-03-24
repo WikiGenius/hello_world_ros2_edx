6946583782b475435894540c9de6f5d19c97bd73
WikiGenius <elyamani.muhammed.eng@gmail.com>
2024-03-24 18:07:29 +0200

    Finish week1_assignment1

--- hrwros_week1/hrwros_assignments/hrwros_week1_assignment/hrwros_week1_assignment/week1_assignment1_part1.py
+++ hrwros_week1/hrwros_assignments/hrwros_week1_assignment/hrwros_week1_assignment/week1_assignment1_part1.py
@@ -3,8 +3,8 @@
 import rclpy
 from rclpy.node import Node
 
-# Import the correct type of message
-from hrwros_msgs.msg import BoxHeightInformation
+from hrwros_msgs.msg import SensorInformation
+
 
 class BoxHeightCalculator(Node):
 
@@ -12,23 +12,27 @@ class BoxHeightCalculator(Node):
         super().__init__('compute_box_height')
         # Create the subscriber for Part1 here
         self.subscription = self.create_subscription(
-            <write-your-code-here-Part1>, '<use correct topic name here>', self.sensor_info_callback, 10)
+            SensorInformation, 'sensor_info', self.sensor_info_callback, 10)
         self.subscription  # prevent unused variable warning
 
     def sensor_info_callback(self, data):
         # Compute the height of the box from the sensor reading.
         # Tip: You need to substract the reading from the max_range of the sensor.
-        height_box = <write-your-code-here-Part1>
+        MAX_RANGE_SENSOR = data.sensor_data.max_range
+        height_box = MAX_RANGE_SENSOR - data.sensor_data.range
 
         # Compute the height of the box.
         # Boxes that are detected to be shorter than 0.1m are due to sensor noise.
         # Do not publish information about them.
-        if <write-your-code-here-Part1>:
+        SHORTER_BOX = 0.1
+        if height_box < SHORTER_BOX:
             pass
+            self.get_logger().info('Filter PASS')
         else:
             # If implemented correctly only the height of boxes bigger than 0.1m
             # will be printed
             self.get_logger().info('Height of box %0.3f' % height_box)
+
 
 def main(args=None):
     rclpy.init(args=args)
@@ -45,6 +49,7 @@ def main(args=None):
         # when the garbage collector destroys the node object)
         box_height_calculator.destroy_node()
         rclpy.shutdown()
+
 
 if __name__ == '__main__':
     main()
--- hrwros_week1/hrwros_assignments/hrwros_week1_assignment/hrwros_week1_assignment/week1_assignment1_part3.py
+++ hrwros_week1/hrwros_assignments/hrwros_week1_assignment/hrwros_week1_assignment/week1_assignment1_part3.py
@@ -3,39 +3,41 @@
 import rclpy
 from rclpy.node import Node
 # Copy the code from Part1 here
-from hrwros_msgs.msg import <write-your-code-here-Part1>
+from hrwros_msgs.msg import SensorInformation
 # Import the correct message type for part 3
-from hrwros_week1_assignment.msg import <write-your-code-here-Part3>
+from hrwros_msgs.msg import BoxHeightInformation
 
 class BoxHeightPublisher(Node):
 
     def __init__(self):
         super().__init__('compute_box_height')
         # Create the publisher for Part3 here
-        self.bhi_publisher = self.create_publisher(<write-your-code-here-Part3>, '<use correct topic name here>', 10)
+        self.bhi_publisher = self.create_publisher(BoxHeightInformation, 'box_height_info', 10)
         # Copy the subscriber from Part1 here, adjusted for ROS 2
         self.subscription = self.create_subscription(
-            <write-your-code-here-Part1>, '<use correct topic name here>', self.sensor_info_callback, 10)
-        self.subscription  # prevent unused variable warning
+            SensorInformation, 'sensor_info', self.sensor_info_callback, 10)
 
     def sensor_info_callback(self, data):
         # Copy the code from Part1 here
-        height_box = <write-your-code-here-Part1>
+        MAX_RANGE_SENSOR = data.sensor_data.max_range
+        height_box = MAX_RANGE_SENSOR - data.sensor_data.range
 
         # Compute the height of the box.
         # Boxes that are detected to be shorter than 10cm are due to sensor noise.
         # Do not publish information about them.
         # Copy the code from Part1 here
-        if <write-your-code-here-Part1>:
+        SHORTER_BOX = 0.1
+        if height_box < SHORTER_BOX:
             pass
+            self.get_logger().info('Filter PASS')
         else:
             # Declare a message object for publishing the box height information.
-            box_height_info = <write-your-code-here-Part3>
+            box_height_info = BoxHeightInformation()
             # Update height of box.
-            <write-your-code-here-Part3>
+            box_height_info.box_height = height_box
             # Publish box height using the publisher argument passed to the
             # callback function.
-            <write-your-code-here-Part3>
+            self.get_logger().info('Height of box %0.3f' % height_box)
             self.bhi_publisher.publish(box_height_info)
 
 def main(args=None):
