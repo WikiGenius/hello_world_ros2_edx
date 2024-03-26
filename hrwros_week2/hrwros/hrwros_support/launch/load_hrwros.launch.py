20c382343c1c893f7c816b9a75486569b012f42c
WikiGenius <elyamani.muhammed.eng@gmail.com>
2024-03-26 05:56:31 +0200

    Add ur_description

--- hrwros_week2/hrwros/hrwros_support/launch/load_hrwros.launch.py
+++ hrwros_week2/hrwros/hrwros_support/launch/load_hrwros.launch.py
@@ -1,24 +1,39 @@
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
-from launch.substitutions import Command, FindExecutable, PathJoinSubstitution
+from launch.substitutions import LaunchConfiguration, Command, FindExecutable, PathJoinSubstitution
 from ament_index_python.packages import get_package_share_directory
 
 
 def generate_launch_description():
-    # Get the package directory
-    pkg_dir = get_package_share_directory('hrwros_support')
+    #  List to hold all declarations for cleaner assembly
+    declare_arguments = [
+        DeclareLaunchArgument(
+            'name',
+            default_value='robot1',
+            description='Name of the arm robot'
+        ),
+        DeclareLaunchArgument(
+            'ur_type',
+            default_value='ur10',
+            description='Type of the arm robot'
+        ),
+        DeclareLaunchArgument(
+            'robot_description',
+            default_value=Command([
+                FindExecutable(name='xacro'),
+                ' ',
+                PathJoinSubstitution([
+                    get_package_share_directory('hrwros_support'),
+                    'urdf',
+                    'hrwros.xacro'
+                ]),
+                ' ',
+                'name:=', LaunchConfiguration('name'),
+                ' ',
+                'ur_type:=', LaunchConfiguration('ur_type')
+            ]),
+            description='Full robot description'
+        ),
+    ]
 
-    # Define the xacro file path
-    xacro_file = PathJoinSubstitution([pkg_dir, 'urdf', 'hrwros.xacro'])
-    
-    robot_description_param = DeclareLaunchArgument(
-        name='robot_description',
-        default_value=Command([
-            FindExecutable(name='xacro'), ' ', xacro_file
-        ]),
-        description='Full robot description'
-    )
-    return LaunchDescription([
-
-        robot_description_param
-    ])
+    return LaunchDescription(declare_arguments)
--- hrwros_week2/hrwros/hrwros_support/launch/visualize_hrwros.launch.py
+++ hrwros_week2/hrwros/hrwros_support/launch/visualize_hrwros.launch.py
@@ -1,32 +1,49 @@
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
-from launch.substitutions import Command, FindExecutable, PathJoinSubstitution, LaunchConfiguration
-from ament_index_python.packages import get_package_share_directory
-
+from launch.conditions import IfCondition, UnlessCondition
+from launch.substitutions import LaunchConfiguration, Command, FindExecutable, PathJoinSubstitution
 from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
 
 
 def generate_launch_description():
-    # Define arguments
-    gui_arg = DeclareLaunchArgument(
-        'gui', default_value='true',
-        description='Flag to enable GUI for joint state publisher.')
+    # List to hold all declarations for cleaner assembly
+    declare_arguments = [
+        DeclareLaunchArgument(
+            'gui',
+            default_value='true',
+            description='Flag to enable GUI for joint state publisher.'
+        ),
+        DeclareLaunchArgument(
+            'name',
+            default_value='robot1',
+            description='Name of the arm robot'
+        ),
+        DeclareLaunchArgument(
+            'ur_type',
+            default_value='ur10',
+            description='Type of the arm robot'
+        ),
+        DeclareLaunchArgument(
+            'robot_description',
+            default_value=Command([
+                FindExecutable(name='xacro'),
+                ' ',
+                PathJoinSubstitution([
+                    get_package_share_directory('hrwros_support'),
+                    'urdf',
+                    'hrwros.xacro'
+                ]),
+                ' ',
+                'name:=', LaunchConfiguration('name'),
+                ' ',
+                'ur_type:=', LaunchConfiguration('ur_type')
+            ]),
+            description='Full robot description'
+        ),
+    ]
 
-    # Get the package directory
-    pkg_dir = get_package_share_directory('hrwros_support')
-
-    # Define the xacro file path
-    xacro_file = PathJoinSubstitution([pkg_dir, 'urdf', 'hrwros.xacro'])
-
-    robot_description_param = DeclareLaunchArgument(
-        name='robot_description',
-        default_value=Command([
-            FindExecutable(name='xacro'), ' ', xacro_file
-        ]),
-        description='Full robot description'
-    )
-
-    # Launch the robot_state_publisher node
+    # # Nodes definition
     robot_state_publisher_node = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
@@ -36,46 +53,41 @@ def generate_launch_description():
             {'robot_description': LaunchConfiguration('robot_description')}]
     )
 
-    # Launch the joint_state_publisher node
-    joint_state_publisher_node = Node(
-        package='joint_state_publisher_gui' if LaunchConfiguration(
-            'gui') == 'true' else 'joint_state_publisher',
-        executable='joint_state_publisher_gui' if LaunchConfiguration(
-            'gui') == 'true' else 'joint_state_publisher',
+    joint_state_publisher_gui_node = Node(
+        package='joint_state_publisher_gui',
+        executable='joint_state_publisher_gui',
         name='joint_state_publisher',
-        parameters=[{
-            'use_gui': LaunchConfiguration('gui'),
-            'robot_description': LaunchConfiguration('robot_description'),
-            'zeros': {
-                'robot1_shoulder_pan_joint': 0.6207787083493841,
-                'robot1_shoulder_lift_joint': -1.004681330618082,
-                'robot1_elbow_joint': 1.6983449885307538,
-                'robot1_wrist_1_joint': -2.301530778020034,
-                'robot1_wrist_2_joint': -1.625460038967466,
-                'robot1_wrist_3_joint': 0.0,
-                'robot2_shoulder_pan_joint': 1.5707963267950005,
-                'robot2_shoulder_lift_joint': -1.5525750894041779,
-                'robot2_elbow_joint': 1.5525750894041783,
-                'robot2_wrist_1_joint': -1.570796326795,
-                'robot2_wrist_2_joint': -1.534353852013356,
-                'robot2_wrist_3_joint': 0.0,
-            }
-        }]
+        condition=IfCondition(LaunchConfiguration('gui')),
+        parameters=[PathJoinSubstitution([
+            get_package_share_directory('hrwros_support'),
+            'config',
+            'zeros.yaml'
+        ])]
     )
-    # Define the xacro file path
-    rviz_file = PathJoinSubstitution([pkg_dir, 'config', 'hrwros.rviz'])
-    # Launch RViz
+
+    joint_state_publisher_node = Node(
+        package='joint_state_publisher',
+        executable='joint_state_publisher',
+        name='joint_state_publisher',
+        condition=UnlessCondition(LaunchConfiguration('gui')),
+        parameters=[{
+            'zeros': LaunchConfiguration('zeros')
+        }],
+    )
     rviz_node = Node(
         package='rviz2',
         executable='rviz2',
         name='rviz',
-        arguments=['-d', rviz_file],
+        arguments=['-d', PathJoinSubstitution(
+            [get_package_share_directory('hrwros_support'), 'config', 'hrwros.rviz'])],
         output='screen',
     )
-    return LaunchDescription([
-        gui_arg,
-        robot_description_param,
+
+    launch_description_elements = declare_arguments + [
         robot_state_publisher_node,
+        joint_state_publisher_gui_node,
         joint_state_publisher_node,
         rviz_node
-    ])
+    ]
+
+    return LaunchDescription(launch_description_elements)
