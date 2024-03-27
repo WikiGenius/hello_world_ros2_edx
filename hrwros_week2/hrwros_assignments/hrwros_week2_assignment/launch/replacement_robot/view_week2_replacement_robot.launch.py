59f73d80e64c0e82a1682be6050368186773666a
WikiGenius <elyamani.muhammed.eng@gmail.com>
2024-03-27 04:53:19 +0200

    Init hrwros_week2_assignment

--- hrwros_week2/hrwros_assignments/hrwros_week2_assignment/launch/replacement_robot/view_week2_replacement_robot.launch.py
+++ hrwros_week2/hrwros_assignments/hrwros_week2_assignment/launch/replacement_robot/view_week2_replacement_robot.launch.py
@@ -2,14 +2,17 @@ from launch import LaunchDescription
 from launch_ros.actions import Node
 from launch.actions import IncludeLaunchDescription, Shutdown
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import ThisLaunchFileDir
+from launch.substitutions import ThisLaunchFileDir, LaunchConfiguration
 from ament_index_python.packages import get_package_share_directory
+from launch_ros.parameter_descriptions import ParameterValue
 import os
+
 
 def generate_launch_description():
     return LaunchDescription([
         IncludeLaunchDescription(
-            PythonLaunchDescriptionSource([ThisLaunchFileDir(), '/replacement_robot/load_week2_robot.launch.py']),
+            PythonLaunchDescriptionSource(
+                [ThisLaunchFileDir(), '/load_week2_robot.launch.py']),
         ),
         Node(
             package='joint_state_publisher_gui',
@@ -21,12 +24,17 @@ def generate_launch_description():
             package='robot_state_publisher',
             executable='robot_state_publisher',
             name='robot_state_publisher',
+                    parameters=[
+                        {'robot_description': ParameterValue(
+                            LaunchConfiguration('robot_description'), value_type=str)},
+                    ]
         ),
         Node(
             package='rviz2',
             executable='rviz2',
             name='rviz',
-            arguments=['-d', os.path.join(get_package_share_directory('hrwros_week2_assignment'), 'config', 'view_robot.rviz')],
+            arguments=['-d', os.path.join(get_package_share_directory(
+                'hrwros_week2_assignment'), 'config', 'view_robot.rviz')],
             output='screen',
             on_exit=Shutdown(),
         ),
