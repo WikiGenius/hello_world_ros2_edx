73db1911df14fd2ef55e1c3c13157f223de0879c
WikiGenius <elyamani.muhammed.eng@gmail.com>
2024-07-05 08:51:58 +0300

    update hrwros_support

---
+++ hrwros_week3/hrwros_support/launch/load_hrwros.launch.py
@@ -0,0 +1,25 @@
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration, Command, FindExecutable, PathJoinSubstitution
+from ament_index_python.packages import get_package_share_directory
+
+
+def generate_launch_description():
+    #  List to hold all declarations for cleaner assembly
+    declare_arguments = [
+        DeclareLaunchArgument(
+            'robot_description',
+            default_value=Command([
+                FindExecutable(name='xacro'),
+                ' ',
+                PathJoinSubstitution([
+                    get_package_share_directory('hrwros_support'),
+                    'urdf',
+                    'hrwros.xacro'
+                ]),
+            ]),
+            description='Full robot description'
+        ),
+    ]
+
+    return LaunchDescription(declare_arguments)
