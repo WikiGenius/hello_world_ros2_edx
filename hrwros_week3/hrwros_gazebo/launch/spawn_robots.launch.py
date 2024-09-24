0d009f79b0d1b02d67b1d5ccc3ca75784ba01e15
WikiGenius <elyamani.muhammed.eng@gmail.com>
2024-09-24 04:41:13 +0300

    Add robot controllers

--- hrwros_week3/hrwros_gazebo/launch/spawn_robots.launch.py
+++ hrwros_week3/hrwros_gazebo/launch/spawn_robots.launch.py
@@ -1,3 +1,5 @@
+import os
+from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument, GroupAction, IncludeLaunchDescription, TimerAction, RegisterEventHandler
 from launch_ros.actions import Node, PushRosNamespace
@@ -6,12 +8,19 @@ from launch_ros.substitutions import FindPackageShare
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.event_handlers import OnProcessExit
 from my_python_utils import some_utility
+# <xacro:arg name="simulation_controllers" default="" />
+# <xacro:arg name="initial_positions_file" default="$(find ur_description)/config/initial_positions.yaml"/>
 
 
 def generate_robot_group(robot_name, robot_type, robot_parent, urdf_file,
                          vacuum_gripper_prefix, gripper_plugin_name,
-                         x, y, z, yaw=None, joints=None):
+                         x, y, z, yaw=None, sim_gazebo=True,
+                         simulation_controllers="", initial_positions_file=os.path.join(get_package_share_directory(
+                             'ur_description'), 'config', 'initial_positions.yaml')):
     """Generate a group of nodes for a single robot."""
+    if simulation_controllers:
+        simulation_controllers = os.path.join(get_package_share_directory(
+                             'hrwros_gazebo'), 'config', simulation_controllers)
     robot_prefix = robot_name
     description_command = some_utility.generate_description_command(
         'hrwros_support',
@@ -20,7 +29,10 @@ def generate_robot_group(robot_name, robot_type, robot_parent, urdf_file,
         f'robot_prefix:={robot_prefix} ',
         f'vacuum_gripper_prefix:={vacuum_gripper_prefix} ',
         f'gripper_plugin_name:={gripper_plugin_name} ',
-        f'robot_parent:={robot_parent} '
+        f'robot_parent:={robot_parent} ',
+        f'sim_gazebo:={sim_gazebo} ',
+        f'simulation_controllers:={simulation_controllers} ',
+        f'initial_positions_file:={initial_positions_file} ',
     )
 
     return GroupAction(
@@ -54,7 +66,7 @@ def generate_launch_description():
             vacuum_gripper_prefix=robot1['vacuum_gripper_prefix'],
             gripper_plugin_name=robot1['gripper_plugin_name'],
             x=robot1['x'], y=robot1['y'], z=robot1['z'],
-            joints=robot1['joints'],
+            simulation_controllers=robot1['simulation_controllers']
         ),
 
         # Group for Robot 2
@@ -66,7 +78,7 @@ def generate_launch_description():
             vacuum_gripper_prefix=robot2['vacuum_gripper_prefix'],
             gripper_plugin_name=robot2['gripper_plugin_name'],
             x=robot2['x'], y=robot2['y'], z=robot2['z'], yaw=robot2['yaw'],
-            joints=robot2['joints'],
+            simulation_controllers=robot2['simulation_controllers']
         ),
 
     ])
