524d1ea07e8aa891fd5b466bea8dff6d79045432
WikiGenius <elyamani.muhammed.eng@gmail.com>
2024-09-30 14:25:57 +0300

    format

--- hrwros_week3/hrwros_gazebo/launch/combined_launch.py
+++ hrwros_week3/hrwros_gazebo/launch/combined_launch.py
@@ -11,6 +11,7 @@ from my_python_utils import helper_combination
 DELAY_SPAWN_TURTLEBOT = 7
 DELAY_SPAWN_NAVIGATION = 10
 
+
 def generate_launch_description():
     # Package directories
     gazebo_ros_share = get_package_share_directory('gazebo_ros')
@@ -18,10 +19,14 @@ def generate_launch_description():
 
     # Paths to world and launch files
     world_path = os.path.join(hrwros_gazebo_share, 'worlds', 'hrwros.world')
-    gzserver_launch_path = os.path.join(gazebo_ros_share, 'launch', 'gzserver.launch.py')
-    gzclient_launch_path = os.path.join(gazebo_ros_share, 'launch', 'gzclient.launch.py')
-    spawn_static_objects_launch_path = os.path.join(hrwros_gazebo_share, 'launch', 'spawn_static_world_objects.launch.py')
-    spawn_robots_launch_path = os.path.join(hrwros_gazebo_share, 'launch', 'spawn_robots.launch.py')
+    gzserver_launch_path = os.path.join(
+        gazebo_ros_share, 'launch', 'gzserver.launch.py')
+    gzclient_launch_path = os.path.join(
+        gazebo_ros_share, 'launch', 'gzclient.launch.py')
+    spawn_static_objects_launch_path = os.path.join(
+        hrwros_gazebo_share, 'launch', 'spawn_static_world_objects.launch.py')
+    spawn_robots_launch_path = os.path.join(
+        hrwros_gazebo_share, 'launch', 'spawn_robots.launch.py')
 
     # Declare simulation-related launch arguments
     launch_args = helper_combination.declare_simulation_arguments()
@@ -31,7 +36,8 @@ def generate_launch_description():
     use_sim_time = LaunchConfiguration('use_sim_time')
     extra_gazebo_args = LaunchConfiguration('extra_gazebo_args')
     debug = LaunchConfiguration('debug')
-    gzserver_launch, gzclient_launch = helper_combination.load_gazebo_launch(gzserver_launch_path, gzclient_launch_path, world_path, extra_gazebo_args, paused, debug)
+    gzserver_launch, gzclient_launch = helper_combination.load_gazebo_launch(
+        gzserver_launch_path, gzclient_launch_path, world_path, extra_gazebo_args, paused, debug)
 
     # Static object and robot spawn launch files
     spawn_static_objects = IncludeLaunchDescription(
@@ -42,8 +48,10 @@ def generate_launch_description():
     )
 
     # Delayed processes for spawning turtlebot and navigation bringup
-    spawn_turtlebot = helper_combination.execute_delayed_process(DELAY_SPAWN_TURTLEBOT, "hrwros_gazebo", "spawn_turtlebot.launch.py")
-    bringup_launch = helper_combination.execute_delayed_process(DELAY_SPAWN_NAVIGATION, "hrwros_nav", "bringup_launch.py")
+    spawn_turtlebot = helper_combination.execute_delayed_process(
+        DELAY_SPAWN_TURTLEBOT, "hrwros_gazebo", "spawn_turtlebot.launch.py")
+    bringup_launch = helper_combination.execute_delayed_process(
+        DELAY_SPAWN_NAVIGATION, "hrwros_nav", "bringup_launch.py")
 
     # Static TF publisher nodes
     tf_static_nodes = helper_combination.create_tf_static_publisher_nodes()
@@ -51,11 +59,13 @@ def generate_launch_description():
     # Twist Mux Node
     cmd_vel_out_arg = LaunchConfiguration('cmd_vel_out')
     config_topics_arg = LaunchConfiguration('config_topics')
-    twist_mux_node = helper_combination.create_twist_mux_node(use_sim_time, config_topics_arg, cmd_vel_out_arg)
+    twist_mux_node = helper_combination.create_twist_mux_node(
+        use_sim_time, config_topics_arg, cmd_vel_out_arg)
 
     # Return LaunchDescription containing all actions
     return LaunchDescription(
-        launch_args + [
+        launch_args +
+        [
             gzserver_launch,
             gzclient_launch,
             spawn_static_objects,
@@ -63,5 +73,6 @@ def generate_launch_description():
             spawn_turtlebot,
             twist_mux_node,
             bringup_launch
-        ] + tf_static_nodes
+        ]
+        + tf_static_nodes
     )
